b7022df362485e5f17ca7d98235b534545cad556
João <jmmsp2003@hotmail.com>
2022-10-30 21:19:26 -0300

    first commit

--- src/main.js
+++ src/main.js
@@ -1,21 +1,21 @@
-console.log('JavaScript code starting...')
+console.log("JavaScript code starting...")
 
-const cabecalho = document.querySelector('#cabecalho')
-document.addEventListener('scroll', () => {
+const cabecalho = document.querySelector("#cabecalho")
+document.addEventListener("scroll", () => {
   const posicao = window.pageYOffset
   if (posicao == 0) {
-    cabecalho.style.backgroundColor = 'transparent'
-    cabecalho.style.backdropFilter = 'blur( 0 )'
+    cabecalho.style.backgroundColor = "transparent"
+    cabecalho.style.backdropFilter = "blur( 0 )"
   } else {
-    cabecalho.style.backgroundColor = 'rgba(0, 0, 0, 0.6)'
-    cabecalho.style.backdropFilter = 'blur( 15px )'
-    cabecalho.style.transition = 'all linear 0.5s'
+    cabecalho.style.backgroundColor = "rgba(0, 0, 0, 0.6)"
+    cabecalho.style.backdropFilter = "blur( 15px )"
+    cabecalho.style.transition = "all linear 0.5s"
   }
 })
 
 function consumirAPI() {
   let url =
-    'https://api.rawg.io/api/games?key=7a244728cfb14f9e91a3d7d899cd0084&dates=2020-01-01,2022-10-27&ordering=-added'
+    "https://api.rawg.io/api/games?key=7a244728cfb14f9e91a3d7d899cd0084&dates=2020-01-01,2022-10-27&ordering=-added"
   fetch(url).then(function (response) {
     response.json().then(function (data) {
       console.log(data)
@@ -25,14 +25,14 @@ function consumirAPI() {
 }
 
 function mostrarTela(dados) {
-  let minhaDiv = document.getElementById('card_api')
+  let minhaDiv = document.getElementById("card_api")
   for (let item = 0; item <= 5; item++) {
-    var elemento = ''
+    var elemento = ""
     elemento += '<div class="cards">'
     elemento += `<img title="${dados.results[item].name}"  src="${dados.results[item].background_image}">`
     elemento += `<p id="nome_jogo">${dados.results[item].name}</p>`
-    elemento += `<p id="avaliacao_jogo">${dados.results[item].rating} <img src="./Public/star.svg" alt=""></p>`
-    elemento += '<h2>Disponível para:</h2>'
+    elemento += `<p id="avaliacao_jogo">${dados.results[item].rating} <img src="Public/star.svg" alt=""></p>`
+    elemento += "<h2>Disponível para:</h2>"
     for (
       let plataforma = 0;
       plataforma <= dados.results[item].parent_platforms.length;
@@ -46,7 +46,7 @@ function mostrarTela(dados) {
       }
     }
     elemento += `<a href="#" id="link_baixar">COMPRAR</a>`
-    elemento += '</div>'
+    elemento += "</div>"
     minhaDiv.innerHTML += elemento
   }
 }
@@ -55,7 +55,7 @@ consumirAPI()
 
 function getLancamentos() {
   let url =
-    'https://api.rawg.io/api/games?key=f3a2f20205384b9189ee65203bfef8c7&dates=2022-08-01,2022-12-30&platforms=18,1,7'
+    "https://api.rawg.io/api/games?key=f3a2f20205384b9189ee65203bfef8c7&dates=2022-08-01,2022-12-30&platforms=18,1,7"
   fetch(url).then(function (response) {
     response.json().then(function (data) {
       console.log(data)
@@ -65,14 +65,14 @@ function getLancamentos() {
 }
 
 function mostrarLancamentos(dados) {
-  let minhaDiv = document.getElementById('card_api_lancamentos')
+  let minhaDiv = document.getElementById("card_api_lancamentos")
   for (let item = 0; item <= 5; item++) {
-    var elemento = ''
+    var elemento = ""
     elemento += '<div class="cards">'
     elemento += `<img title="${dados.results[item].name}" src="${dados.results[item].background_image}">`
     elemento += `<p id="nome_jogo">${dados.results[item].name}</p>`
-    elemento += `<p id="avaliacao_jogo">${dados.results[item].rating} <img src="./Public/star.svg" alt=""></p>`
-    elemento += '<h2>Disponível para:</h2>'
+    elemento += `<p id="avaliacao_jogo">${dados.results[item].rating} <img src="Public/star.svg" alt=""></p>`
+    elemento += "<h2>Disponível para:</h2>"
     for (
       let plataforma = 0;
       plataforma <= dados.results[item].parent_platforms.length;
@@ -86,7 +86,7 @@ function mostrarLancamentos(dados) {
       }
     }
     elemento += `<a href="#" id="link_baixar">COMPRAR</a>`
-    elemento += '</div>'
+    elemento += "</div>"
     minhaDiv.innerHTML += elemento
   }
 }
@@ -95,7 +95,7 @@ getLancamentos()
 
 function getAvaliacoes() {
   let url =
-    'https://api.rawg.io/api/games?key=f3a2f20205384b9189ee65203bfef8c7&dates=2022-01-01,2022-12-31&ordering=-rating'
+    "https://api.rawg.io/api/games?key=f3a2f20205384b9189ee65203bfef8c7&dates=2022-01-01,2022-12-31&ordering=-rating"
   fetch(url).then(function (response) {
     response.json().then(function (data) {
       console.log(data)
@@ -105,14 +105,14 @@ function getAvaliacoes() {
 }
 
 function mostrarAvaliacoes(dados) {
-  let minhaDiv = document.getElementById('card_api_rate')
+  let minhaDiv = document.getElementById("card_api_rate")
   for (let item = 0; item <= 5; item++) {
-    var elemento = ''
+    var elemento = ""
     elemento += '<div class="cards">'
     elemento += `<img title="${dados.results[item].name}" src="${dados.results[item].background_image}">`
     elemento += `<p id="nome_jogo">${dados.results[item].name}</p>`
-    elemento += `<p id="avaliacao_jogo">${dados.results[item].rating} <img src="./Public/star.svg" alt=""></p>`
-    elemento += '<h2>Disponível para:</h2>'
+    elemento += `<p id="avaliacao_jogo">${dados.results[item].rating} <img src="Public/star.svg" alt=""></p>`
+    elemento += "<h2>Disponível para:</h2>"
     for (
       let plataforma = 0;
       plataforma <= dados.results[item].parent_platforms.length;
@@ -126,7 +126,7 @@ function mostrarAvaliacoes(dados) {
       }
     }
     elemento += `<a href="#" id="link_baixar">COMPRAR</a>`
-    elemento += '</div>'
+    elemento += "</div>"
     minhaDiv.innerHTML += elemento
   }
 }
